039510fba4d39675be6bd2d858bb9df520b1a00d
Cyprian Gomes <cypriangomes@Cyprians-MacBook-Air.local>
2022-07-20 17:22:15 +0100

    added prompt for password length

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -17,10 +17,33 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  var specialChars = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  chars = chars + specialChars;
-  var string_length = 12;
+  var string_length = Number(prompt("What would be your password length? The length must be between 8 and 128 characters."));
+  var chars = ''; //= "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  var specialChars = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var numeric = "0123456789";
+  
+  //lowercase check
+  if (1 == 1) {
+    chars = chars + lowerCase;
+  }
+
+  //Uppercase check
+  if (2 == 3) {
+    chars = chars + upperCase;
+  }
+
+  //numeric check
+  if (2 == 2) {
+    chars = chars + numeric;
+  }
+
+  //Sepcial character check
+  if (1 == 0) {
+    chars = chars + specialChars;
+  }
+  
   var randomstring = '';
   for (var i=0; i<string_length; i++) {
       var rnum = Math.floor(Math.random() * chars.length);
@@ -28,3 +51,19 @@ function generatePassword() {
   }
   return randomstring;
 }
+
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
