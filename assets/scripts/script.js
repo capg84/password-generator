9b7856cb1e0dc5ee3d0dcb82b956cbf72383452e
Cyprian Gomes <cypriangomes@Cyprians-MacBook-Air.local>
2022-07-21 09:13:23 +0100

    added minimum character selection validation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,3 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -59,6 +56,11 @@ function generatePassword() {
   } else {
     chars = chars;
   }
+
+//Validation for at least one character set is selected
+  if (lowerCaseIn == 0 && upperCaseIn == 0 && numericIn ==0 && specialCharsIn == 0) {
+    return "At least one type of character must be selected. Please try again.";
+  }
   
   var randomstring = '';
   for (var i=0; i<string_length; i++) {
@@ -67,19 +69,3 @@ function generatePassword() {
   }
   return randomstring;
 }
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
